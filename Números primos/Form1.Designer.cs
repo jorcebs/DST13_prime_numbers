7453edec125858398685521ddbe21f77d4c16707
jorcebs <jor.cebs@gmail.com>
2018-10-20 20:22:32 -0500

    Upload

---
+++ Números primos/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Números_primos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPrimos = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPrimos
+            // 
+            this.txtPrimos.Location = new System.Drawing.Point(37, 48);
+            this.txtPrimos.Multiline = true;
+            this.txtPrimos.Name = "txtPrimos";
+            this.txtPrimos.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPrimos.Size = new System.Drawing.Size(105, 332);
+            this.txtPrimos.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.AutoSize = true;
+            this.btnCalcular.Location = new System.Drawing.Point(43, 12);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(92, 30);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 392);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPrimos);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Primos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPrimos;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Números primos/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Números_primos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            escribePrimos(100);
+        }
+
+        private bool esPrimo(int num)
+        {
+            bool esPrimo = (num > 1) ? true : false;
+
+            for (int i = num - 1; i > 1; i--)
+                if (num % i == 0)
+                {
+                    esPrimo = false;
+                    break;
+                }
+            return esPrimo;
+        }
+
+        private void escribePrimos(int total)
+        {
+            for (int num = 2; total > 0; num++)
+            {
+                if (esPrimo(num))
+                {
+                    txtPrimos.Text += num.ToString() + Environment.NewLine;
+                    total--;
+                }
+            }
+        }
+    }
+}
